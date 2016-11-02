219a305bc54994aecab32821906efe86d36c179f
nicollda <dnicoll@ca.ibm.com>
2016-11-02 11:41:20 -0300

    commit

--- myChaincode.go
+++ myChaincode.go
@@ -117,7 +117,7 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 	a[3] = "100"		//number of shares
 	a[4] = ""
 	a[5] = "BANK"
-	
+	/*
 	_, err = t.registerTrade(stub, "IPO", a)
 	if err != nil {
 		return nil, err
