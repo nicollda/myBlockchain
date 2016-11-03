27142a769639fe0e2a03081f172d3e2a14764e23
nicollda <dnicoll@ca.ibm.com>
2016-11-03 12:30:34 -0300

    commit

--- myChaincode.go
+++ myChaincode.go
@@ -179,7 +179,8 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 	c := []string{"Jamie", "Killed"}
 	c[0] = c[0]
 	
-	/*_, err = t.registerHappening(stub, c)
+	t.registerHappening(stub, c)
+	/*
 	if err != nil {
 		t.writeOut(stub, "in init: after registerHeppeing in err != nil")
 		return nil, err
