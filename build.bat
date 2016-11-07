f02ef8a4fb3ea0d47cdc5d66f5cfdf72606bd6d2
nicollda <dnicoll@ca.ibm.com>
2016-11-07 14:31:26 -0400

    commit

--- build.bat
+++ build.bat
@@ -6,10 +6,8 @@ cls
 ECHO Building.....
 go build ./
 
-echo myChaincodeAPI.go myChaincodeBL.go
-
-
 IF NOT ERRORLEVEL 1 (
+	go clean ./
 	git commit -m "commit" -a
 	git push
 	c:\curl\curl.exe -X POST --insecure --header "Content-Type: application/json" --header "Accept: application/json" -d @deploy.json "https://9f51870d42f942c79c8ab4e217f236d8-vp1.us.blockchain.ibm.com:444/chaincode" > serviceKey.txt
