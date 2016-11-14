5cb912ed8fda23999494a02794d20e21ec114235
nicollda <dnicoll@ca.ibm.com>
2016-11-14 11:27:46 -0400

    commit

--- myChaincodeBL.go
+++ myChaincodeBL.go
@@ -200,6 +200,7 @@ func (t *SimpleChaincode) registerSecurity(securityID string, desc string) ([]by
 
 
 // called by the moderator watson?  to specify that an event happened pay it out
+//todo: need to make dividends payout for each share not just once if there are holdings
 func (t *SimpleChaincode) dividend(securityID string, amount int) ([]byte, error) {
 	fmt.Printf("Running dividend")
 	t.writeOut("in dividend")
