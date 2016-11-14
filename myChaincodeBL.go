c5c5346c1f981d2afacdd8b772122b911c60245e
nicollda <dnicoll@ca.ibm.com>
2016-11-14 14:54:27 -0400

    commit

--- myChaincodeBL.go
+++ myChaincodeBL.go
@@ -34,7 +34,7 @@ import (
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
-const separator = 		"::::"
+const separator = 		"zzzz"
 const userIndex =		"UserIndex" + separator
 const tradeIndex =		"TradeIndex" + separator
 const securityIndex = 	"SecurityIndex" + separator
