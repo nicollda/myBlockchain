5df7cbf6083baecf7dbd59fcd22ca0e4bedaf1c3
nicollda <dnicoll@ca.ibm.com>
2016-11-14 10:47:47 -0400

    commit

--- myChaincodeAPI.go
+++ myChaincodeAPI.go
@@ -138,12 +138,12 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 		return nil, err
 	}
 	
-	/*
+	
 	_, err = t.registerTrade("bid", "Aaron", "Jaime,Killed", defaultPrice, 100, "")
 	if err != nil {
 		return nil, err
 	}
-	
+	/*
 	t.writeOut("Before dividend")
 	//offer payoff anyone with Jaime,Killed (Aaron)
 	_, err = t.dividend("Jaime,Killed", 50)
