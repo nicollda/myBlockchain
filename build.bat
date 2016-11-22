cc73c6c78cba2df1fa30262d49cebd9d43a99d82
nicollda <dnicoll@ca.ibm.com>
2016-11-22 15:26:24 -0400

    commit

--- build.bat
+++ build.bat
@@ -10,7 +10,7 @@ IF NOT ERRORLEVEL 1 (
 	go clean ./
 	git commit -m "commit" -a
 	git push
-	c:\curl\curl.exe -X POST --insecure --header "Content-Type: application/json" --header "Accept: application/json" -d @deploy.json "https://f7ff1a1d892c4bcc95e49b543caae4f4-vp0.us.blockchain.ibm.com:446/chaincode" > serviceKey.txt
+	c:\curl\curl.exe -X POST --insecure --header "Content-Type: application/json" --header "Accept: application/json" -d @deploy.json "https://56cdfd1d1c00471c913f22644988565a-vp2.us.blockchain.ibm.com:5002/chaincode" > serviceKey.txt
 )
 
 
