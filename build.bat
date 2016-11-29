54ba616518492f964e1cce22bf54fc343e65ccc8
nicollda <dnicoll@ca.ibm.com>
2016-11-29 14:58:01 -0400

    commit

--- build.bat
+++ build.bat
@@ -4,10 +4,10 @@ cd c:\gogit
 :build
 cls
 ECHO Building.....
-go build ./
+go build ./myHL/
 
 IF NOT ERRORLEVEL 1 (
-	go clean ./
+	go clean ./myHL/
 	git commit -m "commit" -a
 	git push
 	c:\curl\curl.exe -X POST --insecure --header "Content-Type: application/json" --header "Accept: application/json" -d @deploy.json "https://56cdfd1d1c00471c913f22644988565a-vp2.us.blockchain.ibm.com:5002/chaincode" > serviceKey.txt
